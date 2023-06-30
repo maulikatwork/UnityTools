73e480535df0f4a893f7d447081dfd6fa431ade2
maulikathome <maulikatwork@gmail.com>
2023-07-01 00:17:52 +0530

    Singleton Templet Added

---
+++ SingletonTemplate.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SingletonWithGameobject<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return instance;
+        }
+    }
+
+    
+}
+
+public class SingletonWithoutGameobject<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+               
+            }
+            return instance;
+        }
+    }
+
+
+}
